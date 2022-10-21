9ed85666f46a7d2b76f14c67a78e227b20bbb30d
Hope Ross <hope.ross@daveramsey.com>
2022-10-21 08:49:57 -0500

    second

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Tasks } from '../models/tasks';
 import { TasksService } from '../services/tasks.service';
+import { DialogService } from '../services/dialog.service';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +10,47 @@ import { TasksService } from '../services/tasks.service';
 })
 export class HomePage {
   taskList: Tasks[] = [];
+  newTask: Tasks = new Tasks(0,"temp",false);
   
-  constructor(private tasksService: TasksService, private activatedRoute: ActivatedRoute) {}
+  constructor(private tasksService: TasksService, private dialogService: DialogService) {}
 
   ngOnInit(): void {
     this.tasksService.getAllTasks().subscribe(tasks => {
       this.taskList = tasks;
+    })
+  }
+
+  addNewTask() {
+    this.dialogService.showPrompt('New Task', 'Task Title').subscribe(response => {
+      if(response != null){
+        this.newTask.title = response;
+        this.newTask.completed = false;
+        this.tasksService.createTask(this.newTask).subscribe(() => {
+          window.location.reload();
+        },error => {
+          console.log(`Error: ${error}`);
+        });
+      }
+    });
+  }
+
+  changeCompleted(editTask: Tasks) {
+    if(editTask.completed) {
+      editTask.completed = false;
+      this.tasksService.updateTask(editTask).subscribe(() => {
+        window.location.reload();
+      });
+    } else {
+      editTask.completed = true;
+      this.tasksService.updateTask(editTask).subscribe(() => {
+        window.location.reload();
+      });
+    }
+  }
+
+  deleteTask(taskId: any) {
+    this.tasksService.deleteTask(taskId).subscribe(() => {
+      window.location.reload();
     })
   }
 }
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -2,52 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { Tasks } from '../models/tasks';
-//import { Http } from '@capacitor-community/http';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
-  baseURL: string = "http://localhost:7172/api/tasks"
+  baseURL: string = "https://localhost:7172/api"
 
   constructor(private http: HttpClient) { }
 
   getAllTasks(): Observable<Tasks[]> {
-    return this.http.get<Tasks[]>(`${this.baseURL}`);
+    return this.http.get<Tasks[]>(`${this.baseURL}/tasks`);
+  }
+
+  createTask(newTask: Tasks) {
+    return this.http.post(`${this.baseURL}/tasks`, newTask);
+  }
+
+  getTask(taskId: number) {
+    return this.http.get<Tasks>(`${this.baseURL}/${taskId}`);
+  }
+
+  updateTask(updatedTask: Tasks) {
+    return this.http.put(`${this.baseURL}/tasks/${updatedTask.taskId}`, updatedTask);
+  }
+
+  deleteTask(taskId: number) {
+    return this.http.delete<any>(`${this.baseURL}/tasks/${taskId}`);
   }
 }
-
-
-
-// import { Post } from '../models/post';
-
-
-//   createPost(newPost: Post){
-//     let reqHeaders = {
-//       Authorization: `Bearer ${localStorage.getItem(this.tokenKey)}`
-//     }
-//     return this.http.post(this.baseURL, newPost, {headers: reqHeaders});
-//   }
-
-//   getPost(postId: string) {
-//     let reqHeaders = {
-//       Authorization: `Bearer ${localStorage.getItem(this.tokenKey)}`
-//     }
-//     return this.http.get<Post>(this.baseURL + "/" + postId, {headers: reqHeaders});
-//   }
-
-//   updatePost(updatedPost: Post) {
-//     let reqHeaders = {
-//       Authorization: `Bearer ${localStorage.getItem(this.tokenKey)}`
-//     }
-//     return this.http.put(this.baseURL + "/" + updatedPost.postId, updatedPost, { headers: reqHeaders})
-//   }
-
-//   deletePost(postId: string) {
-//     let reqHeaders = {
-//       Authorization: `Bearer ${localStorage.getItem(this.tokenKey)}`
-//     }
-//     return this.http.delete<any>(this.baseURL + "/" + postId, { headers: reqHeaders});
-//   }
-// }
